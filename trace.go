a60e2ba8ee54b58e201c8fb7b4f57b5833c543fa
soeyusuke <s1240231@gmail.com>
2018-03-28 15:23:58 +0900

    [ADD] maybe ok

--- trace.go
+++ trace.go
@@ -3,15 +3,25 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
+	"net/url"
+	"os"
 	"os/exec"
+	"strings"
 	"sync"
 )
 
 var (
-	wg sync.WaitGroup
+	wg          sync.WaitGroup
+	accessToken = os.Getenv("NOTIFY_TOKEN")
+)
+
+const (
+	endpoint = "https://notify-api.line.me/api/notify"
 )
 
 func main() {
+
 	wg.Add(2)
 	go ProcessHandle("./test")
 	go ProcessHandle("./test2")
@@ -29,4 +39,36 @@ func ProcessHandle(bin string) {
 	if cmd.ProcessState.Exited() != true {
 		log.Print(bin, " is not exited")
 	}
+
+	if err := Notify(bin); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Notify(text string) error {
+	msg := fmt.Sprintf("%s proceess が止まりました。", text)
+
+	u, err := url.ParseRequestURI(endpoint)
+	if err != nil {
+		return err
+	}
+
+	c := &http.Client{}
+	form := url.Values{}
+	form.Add("message", msg)
+
+	body := strings.NewReader(form.Encode())
+	req, err := http.NewRequest("POST", u.String(), body)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Authorization", "Bearer "+accessToken)
+	_, err = c.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
 }
