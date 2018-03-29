2bdf9b15d87bf1494fe3d9ea6b85611ead1490a3
soeyusuke <s1240231@gmail.com>
2018-03-29 17:09:14 +0900

    [FIX]

--- env/init.sh
+++ env/init.sh
@@ -3,7 +3,6 @@ yum install -y git
 wget https://dl.google.com/go/go1.10.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.10.linux-amd64.tar.gz
 
-
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
 echo "export GOPATH=$HOME/go" >> ~/.bashrc
 echo "export PATH=$PATH:$GOPATH/bin" >> ~/.bashrc
