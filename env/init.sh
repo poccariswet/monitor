0514dbd0d6002af38b617e09c26c1981eed5bb6f
soeyusuke <s1240231@gmail.com>
2018-04-02 14:45:46 +0900

    [ADD] init.sh

--- env/init.sh
+++ env/init.sh
@@ -1,8 +1,9 @@
 yum install -y vim
 yum install -y git
+yum install -y nginx
 wget https://dl.google.com/go/go1.10.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.10.linux-amd64.tar.gz
 
-echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
-echo "export GOPATH=$HOME/go" >> ~/.bashrc
-echo "export PATH=$PATH:$GOPATH/bin" >> ~/.bashrc
+# echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
+# echo "export GOPATH=$HOME/go" >> ~/.bashrc
+# echo "export PATH=$PATH:$GOPATH/bin" >> ~/.bashrc
